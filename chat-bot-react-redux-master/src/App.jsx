986f564b74ccf7fd132f6bcc7e75d26f50dcd0df
rajarshi-web <rajarshimandal19@gmail.com>
2023-08-13 13:51:42 +0530

    first-commit

---
+++ chat-bot-react-redux-master/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import routes from './routes/routes';
+import { RouterProvider } from 'react-router-dom';
+function App() {
+  return (
+    <>
+     <RouterProvider router={routes} />
+
+    </>
+  )
+}
+
+export default App
